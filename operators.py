542b889d3e035b1d23c0e9d396a78d61030894f7
Naman Deep <namandeep444@gmail.com>
2025-06-08 19:48:08 +0530

    UV Fix

--- operators.py
+++ operators.py
@@ -942,14 +942,16 @@ class OBJECT_OT_TAMT_UV_Create(bpy.types.Operator):
         for obj in context.selected_objects:
             exist = False
             if obj.type=='MESH':
+                target_uv = None
                 for uv_tex in obj.data.uv_layers:
                     if uv_tex.name==uv_name:
+                        target_uv = uv_tex
                         exist = True
                 if not(exist):
-                    obj.data.uv_layers.new(name=uv_name)
+                    target_uv = obj.data.uv_layers.new(name=uv_name)
                 if mk_active:
-                    obj.data.uv_layers.active=obj.data.uv_layers[uv_name]
-                    obj.data.uv_layers[uv_name].active_render=True
+                    obj.data.uv_layers.active = target_uv
+                    target_uv.active_render=True
                 # if mk_activeRender: 
                 #     obj.data.uv_layers[uv_name].active_render=True
         return {'FINISHED'}    
