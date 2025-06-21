5113cf0dfcee5fa335914ccf74c88ef7ce9b62d0
Naman Deep <namandeep444@gmail.com>
2025-06-21 20:30:44 +0530

    FBX export properties group

--- props.py
+++ props.py
@@ -14,6 +14,257 @@ class CollectionItem(bpy.types.PropertyGroup):
 class exportPresetActive(bpy.types.PropertyGroup):
     selected_preset: bpy.props.EnumProperty(name = "Preset", items =  utils.update_presets )
 
+class fbxExportProperties(bpy.types.PropertyGroup):
+    object_types: bpy.props.EnumProperty(
+        name="Object Types",
+        description="Object Types to Export",
+        options={'ENUM+FLAG'},
+        items=[
+            ('ARMATURE', "Armature", ""),
+            ('CAMERA', "Camera", ""),
+            ('EMPTY', "Empty", ""),
+            ('LIGHT', "Light", ""),
+            ('MESH', "Mesh", ""),
+            ('OTHER', "Other", ""),
+        ],
+        default = {'ARMATURE', 'CAMERA', 'EMPTY', 'LIGHT', 'MESH', 'OTHER'}
+    )
+
+    use_custom_props: bpy.props.BoolProperty(
+        name="Custom Properties",
+        description="Use Custom Prpoerties",
+        default=False,
+    )
+
+
+    # -------------------------------  Transform ------------------------------------------------------
+
+
+
+
+    global_scale: bpy.props.FloatProperty(name="Scale", description="Scale all data (Some importers do not support scaled armatures!)", default=1.0, min=0.001, max=1000)
+
+    apply_unit_scale: bpy.props.BoolProperty(
+        name="Apply Unit Scale",
+        description="Apply Unit Scale",
+        default=True,
+    )
+
+    apply_scale_options: bpy.props.EnumProperty(
+        name="Scale",
+        description="Apply Scalings, How to apply custom and units scalings in generated FBX file (Blender uses FBX scale to detect units on import, but many other applications do not handle the same way)"
+        items=[
+            ('FBX_SCALE_NONE', "All Local", "Apply custom scaling and units scaling to each object transformation, FBX scale remains at 1.0"),
+            ('FBX_SCALE_UNITS', "FBX Units Scale", "Apply custom scaling to each object transformation, and units scaling to FBX scale"),
+            ('FBX_SCALE_CUSTOM', "FBX Custom Scale", "Apply custom scaling to FBX scale, and units scaling to each object transformation")
+            ('FBX_SCALE_ALL', "FBX All", "Apply custom scaling and units scaling to FBX scale"),
+        ],
+        default='FBX_SCALE_NONE'
+    )
+
+    use_space_transform: bpy.props.BoolProperty(
+        name="Use Space Transform",
+        description="Apply global space transform to the object rotations. When disabled only the axis space is written to the file and all object transforms are left as-is",
+        default= True,
+    )
+
+    bake_space_transform: bpy.props.BoolProperty(
+        name= "Apply Transform",
+        description="Bake space transform into object data, avoids getting unwanted rotations to objects when target space is not aligned with Blender’s space (WARNING! experimental option, use at own risk, known to be broken with armatures/animations)"
+    )
+
+    axis_forward: bpy.props.EnumProperty(
+        name="Forward",
+        items=[
+            ('X', "X Forward", ""),
+            ('Y', "Y Forward", ""),
+            ('Z', "Z Forward", ""),
+            ('-X', "-X Forward", ""),
+            ('-Y', "-Y Forward", ""),
+            ('-Z', "-Z Forward", ""),
+        ],
+        default= = '-Z',
+    )
+
+    axis_up: bpy.props.EnumProperty(
+        name="UP",
+        items=[
+            ('X',"X Up", ""),
+            ('Y',"Y Up", ""),
+            ('Z',"Z Up", ""),
+            ('-X',"-X Up", ""),
+            ('-Y',"-Y Up", ""),
+            ('-Z',"-Z Up", ""),
+        ],
+        default='Y',
+    )
+
+
+    # -------------------------------  Geometry ------------------------------------------------------
+
+
+    use_mesh_modifiers: bpy.props.BoolProperty(
+        name="Apply Modifiers",
+        description="Apply modifiers to mesh objects (except Armature ones) - WARNING: prevents exporting shape keys",
+        default=True,
+    )
+
+    mesh_smooth_type: bpy.props.EnumProperty(
+        name="Smoothing",
+        description="Export smoothing information (prefer ‘Normals Only’ option if your target importer understand split normals)",
+        items=[
+            ('OFF',"Normals Only", "Export only normals instead of writing edge or face smoothing data"),
+            ('FACE',"Face", "Write face smoothing"),
+            ('EDGE',"Edge", "Write Edge smoothing"),
+        ],
+        default='OFF',
+    )
+
+    colors_type: bpy.props.EnumProperty(
+        name="Vertex Colors",
+        description="Export vertex color attributes",
+        items=[
+            ('NONE', "None", "Do not export color attributes"),
+            ('SRGB', "sRGB", "Export colors in sRGB color space"),
+            ('LINEAR', "Linear", "Export colors in linear color space"),
+        ],
+        default='SRGB',
+    )
+
+    prioritize_active_color: bpy.props.BoolProperty(
+        name="Prioritize Active Color", 
+        description="Make sure active color will be exported first. Could be important since some other software can discard other color attributes besides the first one",
+        default= False,
+    )
+
+    use_subsurf: bpy.props.BoolProperty(
+        name="Export Subdivision Surface",
+        description="Export the last Catmull-Rom subdivision modifier as FBX subdivision (does not apply the modifier even if ‘Apply Modifiers’ is enabled)",
+        default=False,
+    )
+
+    use_mesh_edges: bpy.props.BoolProperty(
+        name="Loose Edges",
+        description="Export loose edges (as two-vertices polygons)",
+        default=False,
+    )
+    use_tspace: bpy.props.BoolProperty(
+        name="Tangent Space",
+        description="Add binormal and tangent vectors, together with normal they form the tangent space (will only work correctly with tris/quads only meshes!)",
+        default=False,
+    )
+    use_triangles: bpy.props.BoolProperty(
+        name="Triangulate Faces",
+        description="Convert all faces to triangles",
+        default=False,
+    )
+
+    # -------------------------------  Armature ------------------------------------------------------
+
+
+
+    add_leaf_bones: bpy.props.BoolProperty(
+        name="Add Leaf Bones",
+        description="Append a final bone to the end of each chain to specify last bone length (use this when you intend to edit the armature from exported data)",
+        default=True,
+    )
+
+    primary_bone_axis: bpy.props.EnumProperty(
+        name="Primary Bone Axis",
+        items=[
+            ('X', "X Axis", ""),
+            ('Y', "Y Axis", ""),
+            ('Z', "Z Axis", ""),
+            ('-X', "-X Axis", ""),
+            ('-Y', "-Y Axis", ""),
+            ('-Z', "-Z Axis", ""),
+        ],
+        default='Y',
+    )
+
+    secondary_bone_axis: bpy.props.EnumProperty(
+        name="Secondary Bone Axis",
+        items=[
+            ('X', "X Axis", ""),
+            ('Y', "Y Axis", ""),
+            ('Z', "Z Axis", ""),
+            ('-X', "-X Axis", ""),
+            ('-Y', "-Y Axis", ""),
+            ('-Z', "-Z Axis", ""),
+        ],
+        default='X',
+    )
+
+    use_armature_deform_only: bpy.props.BoolProperty(
+        name="Only Deform Bones",
+        description="Only write deforming bones (and non-deforming ones when they have deforming children)",
+        default=False,
+    )
+
+    armature_node_type: bpy.props.EnumProperty(
+        name="Armature FBX Node Type",
+        description="FBX type of node (object) used to represent Blender’s armatures (use the Null type unless you experience issues with the other app, as other choices may not import back perfectly into Blender…)",
+        items=[
+            ('NULL', "Null", "‘Null’ FBX node, similar to Blender’s Empty (default)"),
+            ('ROOT', "Root", "‘Root’ FBX node, supposed to be the root of chains of bones…."),
+            ('LIMBNODE',"LimbNode", "‘LimbNode’ FBX node, a regular joint between two bones…."),
+        ],
+        default= 'NULL',
+    )
+
+
+    # -------------------------------  Animation ------------------------------------------------------
+
+    
+    bake_anim: bpy.props.BoolProperty(
+        name="Baked Animation",
+        description = "Export baked keyframe animation",
+        default=False,
+    )
+
+    bake_anim_use_all_bones: bpy.props.BoolProperty(
+        name="Key All Bones",
+        description = "Force exporting at least one key of animation for all bones (needed with some target applications, like UE4)",
+        default=True,
+    )
+
+    bake_anim_use_nla_strips: bpy.props.BoolProperty(
+        name="NLA Strips",
+        description = "Export each non-muted NLA strip as a separated FBX’s AnimStack, if any, instead of global scene animation",
+        default=True,
+    )
+
+    bake_anim_use_all_actions: bpy.props.BoolProperty(
+        name="All Actions",
+        description = "Export each action as a separated FBX’s AnimStack, instead of global scene animation (note that animated objects will get all actions compatible with them, others will get no animation at all)",
+        default=True,
+    )
+
+    bake_anim_force_startend_keying: bpy.props.BoolProperty(
+        name="Force Start/End Keying",
+        description = "Always add a keyframe at start and end of actions for animated channels",
+        default=True,
+    )
+
+    bake_anim_step: bpy.props.FloatProperty(
+        name="Sampling Rate",
+        description = "How often to evaluate animated values (in frames)",
+        default=1.00,
+        min=0.01,
+        max=100,
+    )
+    bake_anim_simplify_factor: bpy.props.FloatProperty(
+        name="Simplify",
+        description = "How much to simplify baked values (0.0 to disable, the higher the more simplified)",
+        default=1.00,
+        min=0,
+        max=100,
+    )
+
+
+
+
+
 
 class exportProperties(bpy.types.PropertyGroup):
 
